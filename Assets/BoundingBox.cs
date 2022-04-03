daf5c71f0ee30aef7ba40483622ac4583cf215f9
Jim Magnusson <jim.magnusson97@gmail.com>
2022-04-03 23:05:09 +0200

    BoundingBox

---
+++ Assets/BoundingBox.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundingBox : MonoBehaviour
+{
+    LevelLoader levelLoader;
+    private void Start()
+    {
+        levelLoader = FindObjectOfType<LevelLoader>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Player out of bounds");
+        if (levelLoader != null)
+        {
+            levelLoader.ReloadScene();
+        }
+    }
+}
---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadSceneWithBuildIndex(int buildindex)
+    {
+        SceneManager.LoadScene(buildindex);
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            ReloadScene();
+        }
+    }
+}
